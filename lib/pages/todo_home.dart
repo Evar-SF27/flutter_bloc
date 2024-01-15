6c379fcbc47c22dde52f3ff1ac2aad3c0e3923b4
Evar-SF27 <evarfomuso@outlook.com>
2024-01-15 21:52:01 +0100

    weather UI

--- lib/pages/todo_home.dart
+++ lib/pages/todo_home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:state_manager/cubit/todo_cubit.dart';
 import 'package:state_manager/models/todo_model.dart';
 import 'package:state_manager/pages/todo_add.dart';
+import 'package:state_manager/pages/weather.dart';
 
 class TodoHome extends StatelessWidget {
   const TodoHome({super.key});
@@ -10,7 +11,14 @@ class TodoHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Todo List')),
+        appBar: AppBar(title: const Text('Todo List'), actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: ((context) => const WeatherScreen())));
+              },
+              icon: const Icon(Icons.light_mode))
+        ]),
         body: BlocBuilder<TodoCubit, List<Todo>>(builder: (context, todos) {
           return ListView.builder(
               itemCount: todos.length,
