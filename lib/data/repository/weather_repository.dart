0550a8078277e8ff1f9c3d59a6f109121d66150c
Evar-SF27 <evarfomuso@outlook.com>
2024-01-15 23:10:11 +0100

    weather repo and provider

---
+++ lib/data/repository/weather_repository.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:state_manager/data/provider/weather_data_provider.dart';
+import 'package:state_manager/models/weather_model.dart';
+
+class WeatherRepository {
+  final WeatherDataProvider weatherDataProvider;
+  WeatherRepository(this.weatherDataProvider);
+
+  Future<WeatherModel> getCurrentWeather() async {
+    try {
+      String cityName = 'Buea';
+      final weatherData = await weatherDataProvider.getCurrentWeather(cityName);
+
+      final data = jsonDecode(weatherData);
+
+      if (data['cod'] != '200') {
+        throw 'An unexpected error occured';
+      }
+
+      return WeatherModel.fromMap(data);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
